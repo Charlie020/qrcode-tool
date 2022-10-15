9aee30aa2ad0e582f5a3a8003fafda5484667cca
25803 <2580390720@qq.com>
2022-10-15 14:26:35 +0800

    Generate

--- app/src/main/java/com/example/qrcode/Generate.java
+++ app/src/main/java/com/example/qrcode/Generate.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Generate extends AppCompatActivity {
