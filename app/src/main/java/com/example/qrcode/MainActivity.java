ec929af6d4307d442380f41acba7264633dc0fe9
Talon <1604690196@qq.com>
2022-10-21 15:55:24 +0800

    增加注释

--- app/src/main/java/com/example/qrcode/MainActivity.java
+++ app/src/main/java/com/example/qrcode/MainActivity.java
@@ -1,36 +1,20 @@
 package com.example.qrcode;
 
-
-
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.Manifest;
-import android.app.Activity;
 import android.app.AlertDialog;
-import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.os.Bundle;
-
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-import com.google.zxing.common.BitMatrix;
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
-import com.journeyapps.barcodescanner.CaptureActivity;
-
-
-
 
 
 public class MainActivity extends AppCompatActivity {
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
         StatusBar statusBar = new StatusBar(MainActivity.this);
         statusBar.setStatusBarColor(R.color.transparent);
 
+        //设置当前界面UI样式
         setContentView(R.layout.activity_main);
 
         //去除默认标题栏
@@ -47,13 +32,14 @@ public class MainActivity extends AppCompatActivity {
         }
 
         Button generate = this.findViewById(R.id.Generate);    //生成二维码
-        generate.setOnClickListener(new changeXmlListener());//切换页面 开启新的activity
-        Button generate2 = this.findViewById(R.id.Scan);
-        generate2.setOnClickListener(new View.OnClickListener() {
+        generate.setOnClickListener(new changeXmlListener());  //切换页面 开启新的activity
+
+        Button scan = this.findViewById(R.id.Scan);    //扫描二维码
+        scan.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 IntentIntegrator intentIntegrator =new IntentIntegrator(MainActivity.this);
-                intentIntegrator.setPrompt("闪光");
+                intentIntegrator.setPrompt("请将识别框对准二维码");
                 intentIntegrator.setBeepEnabled(true);
                 intentIntegrator.setOrientationLocked(true);
                 intentIntegrator.setCaptureActivity(Capture.class);
@@ -61,10 +47,17 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    //消息回传  处理扫描界面获得的二维码信息并在当前活动进行处理
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        IntentResult intentResult=IntentIntegrator.parseActivityResult(requestCode,resultCode,data);
+
+        IntentResult intentResult=IntentIntegrator.parseActivityResult(requestCode,resultCode,data);  //获取回传信息
+
+        Toast toast=Toast.makeText(getApplicationContext(), intentResult.getContents(), Toast.LENGTH_SHORT);
+        toast.show();
+
         if (intentResult.getContents()!=null){
             AlertDialog.Builder builder=new AlertDialog.Builder(MainActivity.this);
             builder.setTitle("Result");
@@ -76,14 +69,13 @@ public class MainActivity extends AppCompatActivity {
                 }
             });
             builder.show();
-        }else {
-            Toast.makeText(getApplicationContext(),"OOPS... Did not scan anything",Toast.LENGTH_SHORT).show();
         }
     }
 
     public class changeXmlListener implements View.OnClickListener {
+        //切换界面的类
         public void onClick(View v) {
-            Intent intent = new Intent(MainActivity.this, TwoActivity.class);
+            Intent intent = new Intent(MainActivity.this, TwoActivity.class);  //intent是Android里用于activity之间信息传递的类
             startActivity(intent);
         }
     }
--- app/src/main/java/com/example/qrcode/StatusBar.java
+++ app/src/main/java/com/example/qrcode/StatusBar.java
@@ -21,26 +21,5 @@ public class StatusBar {
             activity.getWindow().setStatusBarColor(activity.getResources().getColor(color));
         }
     }
-
-    //隐藏状态栏
-    public void hideStatusBar(){
-        if (Build.VERSION.SDK_INT >= 21) {
-            activity.getWindow()
-                    .setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        }
-    }
-
-    //设置状态栏字体颜色
-    public void setTextColor(boolean isDarkBackground){
-        View decor = activity.getWindow().getDecorView();
-        if (isDarkBackground) {
-            //黑暗背景字体浅色
-            decor.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
-        } else {
-            //高亮背景字体深色
-            decor.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
-        }
-    }
-
 }
 
