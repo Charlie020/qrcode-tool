cabc8da363bf73c7abb6cce24c452fe7fd7d50d3
Talon <1604690196@qq.com>
2022-10-22 11:15:59 +0800

    增加了文本识别功能 纯文本用阅读器输出

--- app/src/main/java/com/example/qrcode/MainActivity.java
+++ app/src/main/java/com/example/qrcode/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.qrcode;
 
 import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
@@ -10,6 +13,9 @@ import android.widget.Button;
 
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -56,7 +62,9 @@ public class MainActivity extends AppCompatActivity {
 
         IntentResult intentResult=IntentIntegrator.parseActivityResult(requestCode,resultCode,data);  //获取回传信息
 
-        if (intentResult.getContents()!=null){
+        boolean isURL=new RegexUtill().verifyUrl(intentResult.getContents());
+
+        if (isURL){
             //浏览器部分  增加网络权限
             String uriString=intentResult.getContents();   //获取URL
             Intent intent = new Intent();
@@ -64,7 +72,20 @@ public class MainActivity extends AppCompatActivity {
             Uri content_url = Uri.parse(uriString);
             intent.setData(content_url);
             startActivity(intent);
-
+        }
+        else {
+            //不是URL 直接将文本输出
+            //使用一个文本确认框的形式
+            AlertDialog.Builder builder=new AlertDialog.Builder(MainActivity.this);
+            builder.setTitle("文本内容：");
+            builder.setMessage(intentResult.getContents());
+            builder.setPositiveButton("退出", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    dialogInterface.dismiss();
+                }
+            });
+            builder.show();
         }
     }
 
@@ -73,6 +94,21 @@ public class MainActivity extends AppCompatActivity {
         public void onClick(View v) {
             Intent intent = new Intent(MainActivity.this, InputActivity.class);  //intent是Android里用于activity之间信息传递的类
             startActivity(intent);
+        }
+    }
+
+    public class RegexUtill {
+        //判断字符串是否是URL的类
+
+        public boolean verifyUrl(String url){
+            // URL验证规则
+            String regEx ="^([hH][tT]{2}[pP]://|[hH][tT]{2}[pP][sS]://)(([A-Za-z0-9-~]+).)+([A-Za-z0-9-~/])+$";
+            // 编译正则表达式
+            Pattern pattern = Pattern.compile(regEx);
+
+            Matcher matcher = pattern.matcher(url);
+            // 字符串是否与正则表达式相匹配
+            return matcher.matches();
         }
     }
 }
